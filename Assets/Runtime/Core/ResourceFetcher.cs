f20da3d1fd21ddb1dd956b450979fa8816f61a0d
RedLinesNT <77906555+RedLinesNT@users.noreply.github.com>
2023-08-23 02:02:24 +0200

    ResourceFetcher

---
+++ Assets/Runtime/Core/ResourceFetcher.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+namespace Oikos.Core {
+    
+    /// <summary>
+    /// This class allow to fetch "ScriptableObject" files at runtime.
+    /// The files fetched should only be used to read content to avoid GameData modifications at runtime.
+    /// </summary>
+    public static class ResourceFetcher {
+        
+        /// <summary>
+        /// Scan through every files in the Resources/ folder and returns the files from the type asked
+        /// </summary>
+        /// <typeparam name="T">File type</typeparam>
+        public static T[] GetResourceFilesFromType<T>() where T : Object {
+            return Resources.LoadAll<T>("");
+        }
+
+        /// <summary>
+        /// Scan through every files in the Resources/ folder and returns the file at a specific index from the list of files found
+        /// </summary>
+        /// <typeparam name="T">File type</typeparam>
+        public static T GetResourceFileFromTypeWithIndex<T>(int _index) where T : Object {
+            T[] _tempResult = Resources.LoadAll<T>("");
+
+            return _tempResult[_index] == null ? _tempResult[0] : _tempResult[_index];
+        }
+
+        /// <summary>
+        /// Scan through every files in the Resources/ folder and returns the file from the type asked
+        /// </summary>
+        /// <typeparam name="T">File type</typeparam>
+        public static T GetResourceFileFromType<T>() where T : Object {
+            return Resources.LoadAll<T>("")[0];
+        }
+        
+    }
+    
+}
