a01795e083c2f4240e6d7d7b9e455664dfa46555
RedLinesNT <77906555+RedLinesNT@users.noreply.github.com>
2023-08-31 19:52:57 +0200

    Object Creator (Editor)

---
+++ Assets/Editor/Runtime/ObjectsCreator.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using Oikos.Data;
+using UnityEditor;
+using UnityEngine;
+
+namespace Oikos.Editor {
+    
+    /// <summary>
+    /// Creates ScriptableObjects instances as files inside the Editor
+    /// </summary>
+    public static class ObjectsCreator {
+        
+        /// <summary>
+        /// Create a TrashObjectData (ScriptableObject) file
+        /// </summary>
+        [MenuItem("Oikos/Gameplay/Trash Object file")] public static void CreateTrashObjectFile() {
+            TrashObjectData _asset = ScriptableObject.CreateInstance<TrashObjectData>();
+            
+            if(!Directory.Exists("Assets/Resources/Gameplay/Trash Objects/")) { //Check if the directory exists
+                Directory.CreateDirectory("Assets/Resources/Gameplay/Trash Objects/");
+            }
+            
+            string _assetPath = AssetDatabase.GenerateUniqueAssetPath("Assets/Resources/Gameplay/Trash Objects/New Object.asset");
+            AssetDatabase.CreateAsset(_asset, _assetPath);
+            AssetDatabase.SaveAssets();
+            
+            EditorUtility.FocusProjectWindow();
+            
+            Selection.activeObject = _asset;
+        }
+        
+    }
+    
+}
--- Assets/Runtime/Data/TrashObjectData.cs
+++ Assets/Runtime/Data/TrashObjectData.cs
@@ -3,18 +3,20 @@ using UnityEngine;
 
 namespace Oikos.Data {
 
-    public class TrashObjectDataContent : ScriptableObject {
+    public class TrashObjectData : ScriptableObject {
 
         #region Attributes
 
-        [Header("Trash Item References")]
+        [Header("Trash Item Definition")]
         [SerializeField] private E_TRASH_OBJECT_TYPE identifier;
+        [SerializeField] private E_TRASHBIN_IDENTIFIER trashBinIdentifier;
+        [SerializeField] private Sprite objectIconSprite;
+        [SerializeField] private GameObject objectPrefab;
+        
+        [Header("Translation references")]
         [SerializeField] private string nameTranslationIdentifierString;
         [SerializeField] private string descriptionTranslationIdentifierString;
         [SerializeField] private string worldDamageDescriptionTranslationIdentifierString;
-        [SerializeField] private E_TRASH_OBJECT_TYPE trashBinIdentifier;
-        [SerializeField] private Sprite objectIconSprite;
-        [SerializeField] private GameObject objectPrefab;
 
         #endregion
         
