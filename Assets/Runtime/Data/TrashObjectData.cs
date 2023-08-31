90c44fe0f39cbaef009520be5e4fe3a149658637
RedLinesNT <77906555+RedLinesNT@users.noreply.github.com>
2023-09-01 00:59:51 +0200

    Trash Object Data (String data/Localization include)

--- Assets/Runtime/Data/TrashObjectData.cs
+++ Assets/Runtime/Data/TrashObjectData.cs
@@ -8,16 +8,62 @@ namespace Oikos.Data {
 
         #region Attributes
 
+        [SerializeField, Tooltip("The internal name of this item.")] private string internalItemName = null;
+        
         [Header("Trash Item Definition")]
-        [SerializeField] private E_TRASH_OBJECT_TYPE identifier;
-        [SerializeField] private E_TRASHBIN_IDENTIFIER trashBinIdentifier;
-        [SerializeField] private Sprite objectIconSprite;
-        [SerializeField] private GameObject objectPrefab;
+        [SerializeField, Tooltip("The identifier of this trash object.")] private E_TRASH_OBJECT_TYPE identifier = E_TRASH_OBJECT_TYPE.PLACEHOLDER;
+        [SerializeField, Tooltip("The identifier of the target trash bin of this object.")] private E_TRASHBIN_IDENTIFIER trashBinIdentifier = E_TRASHBIN_IDENTIFIER.PLACEHOLDER;
+        [SerializeField, Tooltip("The sprite icon of this trash object.")] private Sprite trashIconSprite = null;
+        [SerializeField, Tooltip("The Prefab of this trash object.")] private GameObject trashModelPrefab = null;
         
         [Header("Translation references")]
-        [SerializeField] private LocalizedString nameTranslationIdentifierString;
-        [SerializeField] private LocalizedString descriptionTranslationIdentifierString;
-        [SerializeField] private LocalizedString worldDamageDescriptionTranslationIdentifierString;
+        [SerializeField, Tooltip("The trash's name (Localized string).")] private LocalizedString trashNameLocalizedString = null;
+        [SerializeField, Tooltip("The trash's type name (Localized string).")] private LocalizedString trashTypeLocalizedString = null;
+        [SerializeField, Tooltip("The trash's lifespan (Localized string).")] private LocalizedString trashLifespanLocalizedString = null;
+        [SerializeField, Tooltip("The trash's world impact description (Localized string).")] private LocalizedString worldImpactDescriptionLocalizedString = null;
+        
+        #endregion
+
+        #region Properties
+        
+        /// <summary>
+        /// The internal name of this trash item.
+        /// </summary>
+        public string InternalName { get { return internalItemName; } }
+
+        /// <summary>
+        /// The identifier of this item. Defined by the "E_TRASH_OBJECT_TYPE" Enum.
+        /// </summary>
+        public E_TRASH_OBJECT_TYPE Identifier { get { return identifier; } }
+        /// <summary>
+        /// The identifier of the target trash bin of this item. Defined by the "E_TRASHBIN_IDENTIFIER" Enum.
+        /// </summary>
+        public E_TRASHBIN_IDENTIFIER TrashbinIdentifier { get { return trashBinIdentifier; } }
+        /// <summary>
+        /// The icon of this trash object.
+        /// </summary>
+        public Sprite Icon { get { return trashIconSprite; } }
+        /// <summary>
+        /// The model prefab of this trash object.
+        /// </summary>
+        public GameObject ModelPrefab { get { return trashModelPrefab; } }
+        
+        /// <summary>
+        /// The name of this trash object (This string is already translated in the correct language).
+        /// </summary>
+        public string NameLocalizedString { get { return trashNameLocalizedString.GetLocalizedString(); } }
+        /// <summary>
+        /// The name of the trash type of this trash object (This string is already translated in the correct language).
+        /// </summary>
+        public string TrashTypeLocalizedString { get { return trashTypeLocalizedString.GetLocalizedString(); } }
+        /// <summary>
+        /// The lifespan of this trash object (This string is already translated in the correct language).
+        /// </summary>
+        public string LifespanLocalizedString { get { return trashLifespanLocalizedString.GetLocalizedString(); } }
+        /// <summary>
+        /// The world impact's description of this trash object (This string is already translated in the correct language).
+        /// </summary>
+        public string WorldImpactDescriptionLocalizedString { get { return worldImpactDescriptionLocalizedString.GetLocalizedString(); } }
         
         #endregion
         
