5ebe40d2c97596eb646c81696b2b9cec8b520058
RedLinesNT <77906555+RedLinesNT@users.noreply.github.com>
2023-09-16 15:16:31 +0200

    UI Widget references/Light baking data

---
+++ Assets/Runtime/Types/UIWidgetType.cs
@@ -0,0 +1,14 @@
+﻿namespace Oikos.Types {
+    
+    /// <summary>
+    /// Contain the UI Widget types
+    /// </summary>
+    public enum E_UI_WIDGET_TYPE {
+        /// <summary>
+        /// Do not use this type
+        /// </summary>
+        UNKNOWN = 0,
+        TRASH_OBJECT_DIALOGUE_WORLD_IMPACT_DESC,
+    }
+    
+}
